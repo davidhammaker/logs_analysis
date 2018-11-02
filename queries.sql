5ff2f159d5e17e3f04b711c82c68e109a51b263c
David J. Hammaker <davidjosh93@gmail.com>
2018-11-02 16:31:40 -0400

    Update first query to group by articles.id
    The first query originally grouped rows by article titles, which are not
    necessarily unique. If two articles had the same title, the query would
    group them together. However, ID numbers are unique.

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 select articles.title, count(*) as total
     from log, articles
     where log.path = concat('/article/', articles.slug)
-    group by articles.title
+    group by articles.id
     order by total
     desc limit 3;
 
