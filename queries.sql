198c84047068c40569848a8e7e6a2722dd86b132
David J. Hammaker <davidjosh93@gmail.com>
2018-11-02 16:19:16 -0400

    Add second query to queries.sql
    This query attempts to select the three "most popular article authors of
    all time" from the database. This query selects author names and 'total'
    rows for each unique author id number. The query selects this data from
    all three tables: 'log', 'articles', and 'authors'. Like the first
    query, 'articles' and 'log' are joined by comparing log.path to
    'articles.slug'. Additionally, 'authors' is joined to 'articles' by
    comparing authors.id to articles.author, both of which are unique
    integers per author. Rows are grouped by unique author id numbers. Like
    the first query, this query orders its results by 'total' rows in
    descending order. Results are limited to 3 rows.

--- queries.sql
+++ queries.sql
@@ -4,3 +4,11 @@ select articles.title, count(*) as total
     group by articles.title
     order by total
     desc limit 3;
+
+select authors.name, count(*) as total
+    from authors, log, articles
+    where log.path = concat('/article/', articles.slug)
+    and authors.id = articles.author
+    group by authors.id
+    order by total desc
+    limit 3;
