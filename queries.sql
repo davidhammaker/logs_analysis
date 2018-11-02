58d3af44288a816606f96bbd875b1ff8b58ce8c3
David J. Hammaker <davidjosh93@gmail.com>
2018-11-02 18:13:04 -0400

    Add third query, version 1
    This query is incomplete. It is the first draft of a query that will
    answer the question, "On which days did more than 1% of requests lead to
    errors?" The query joins two subqueries. The first subquery selects the
    date (truncating the timestamp to the day) and the count of all requests
    errors per each date. The second subquery selects the date (also
    truncating the timestamp) and the count of all requests. Both subqueries
    select from the 'log' table. The query joins the subqueries by date. The
    query selects the date, error count, and total count from the joining of
    the subqueries. So far, the query does not accomplish more than this. In
    the future, the error count may be divided by the total count to return
    the percentage of errors per day.

--- queries.sql
+++ queries.sql
@@ -12,3 +12,18 @@ select authors.name, count(*) as total
     group by authors.id
     order by total desc
     limit 3;
+
+select total_log.date, error_log.errors, total_log.total
+    from
+    (select date_trunc('day', time) as date,
+            count(*) as errors
+        from log
+        where status = '404 NOT FOUND'
+        group by date)
+        as error_log
+    join
+    (select date_trunc('day', time) as date,
+            count(*) as total
+        from log
+        group by date) as total_log
+    on error_log.date = total_log.date;
