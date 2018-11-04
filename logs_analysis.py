1a4d2db9497e133146831d79cebe89140b842ce2
David J. Hammaker <davidjosh93@gmail.com>
2018-11-03 20:21:49 -0400

    Add docstrings
    One docstring for each function, plus a docstring for the entire
    logs_analysis.py file.

--- logs_analysis.py
+++ logs_analysis.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 """
-
+This code performs an analysis on a database of HTTP request logs on from a
+news website. The code uses psycopg2 as its PostgreSQL adapter. The code
+contains three functions, each of which performs a single SQL query. The
+results of these queries are printed to the console and to a text file.
 """
 
 import psycopg2
 
 
 def first_query():
+    """
+    Answer the following question:
+        "What are the most popular three articles of all time?"
+
+    Return:
+        A list of three tuples, each of which contains the name of an article
+        and its total number of views, listed in descending order.
+    """
     db = psycopg2.connect("dbname=news")
     c = db.cursor()
     q = '''
@@ -24,6 +35,14 @@ def first_query():
 
 
 def second_query():
+    """
+    Answer the following question:
+        "Who are the most popular article authors of all time?"
+
+    Return:
+        A list of tuples, each of which contains the name of an author and
+        his/her total number of views, listed in descending order.
+    """
     db = psycopg2.connect("dbname=news")
     c = db.cursor()
     q = '''
@@ -41,6 +60,15 @@ def second_query():
 
 
 def third_query():
+    """
+    Answer the following question:
+        "On which days did more than 1% of requests lead to errors?"
+
+    Return:
+        A list of tuples, each of which contains a timestamp with time zone
+        (truncated to the day) and a number representing the percentage
+        (rounded to the nearest 10th's place), listed in descending order.
+    """
     db = psycopg2.connect("dbname=news")
     c = db.cursor()
     q = '''
